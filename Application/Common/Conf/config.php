32b0bad7994b10f56d7229e73eb37a0059eb6a2d
Yinan Zhou <zhouyinan@efzer.org>
2016-06-24 23:18:52 +0800

    Create Common Module
    Signed-off-by: Yinan Zhou <zhouyinan@efzer.org>

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	'COOKIE_PREFIX'         =>  'efzer_qagame_',
+	'URL_MODEL'             =>  2,
+	'URL_HTML_SUFFIX'       =>  '',
+	'TMPL_TEMPLATE_SUFFIX'  =>  '.html',
+	'DB_TYPE'               =>  'mysql',
+	'SHOW_ERROR_MSG'        =>  true,
+  'MODULE_DENY_LIST'=>  array('Common','Runtime'),
+  'MULTI_MODULE'          =>  true,
+);
