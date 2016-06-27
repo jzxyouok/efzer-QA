df1cd36660326479394902d3c5c77ebb4f7827d6
Yinan Zhou <zhouyinan@efzer.org>
2016-06-28 02:58:03 +0800

    Add activity statistics feature to Admin Dashboard

--- Application/Admin/Controller/DashboardController.class.php
+++ Application/Admin/Controller/DashboardController.class.php
@@ -3,6 +3,15 @@ namespace Admin\Controller;
 use Think\Controller;
 class DashboardController extends Controller{
   public function index(){
+    $Question = D('Questions');
+    $Submission = D('Submissions');
+    $Response = D('Responses');
+    $submissions_count = $Submission->count();
+    $this->assign('submissions_count',$submissions_count);
+    $this->assign('questions_count',$Question->count());
+    if($submissions_count == 0){
+      $this->assign('responses_correct_average','N/A');
+    }
     $this->display();
   }
 }
