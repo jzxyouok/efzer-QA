0af4b7c9ab4c4d5c96bcb4b81163567e5f8c4326
Yinan Zhou <zhouyinan@efzer.org>
2016-07-02 11:52:48 +0800

    Add ADMIN_PASSWORD to config
    ADMIN_PASSWORD config field is added to config.php. Also more comments are
    added to config.php

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,8 +1,28 @@
 <?php
 return array(
+	// 活动名称
 	'ACTIVITY_NAME' => '二附中人问答挑战',
+	// 活动主办者
 	'ACTIVITY_HOLDER' => '华东师大二附中校友联络会',
+	// 管理员密码
+	'ADMIN_PASSWORD' => 'admin',
+	// Cookie前缀
 	'COOKIE_PREFIX'         =>  'efzer_qagame_',
+
+	// 数据库设置
+	'DB_TYPE' => 'mysql', //数据库类型
+	'DB_HOST' => 'localhost', //数据库地址
+	'DB_NAME' => '', // 数据库名
+  'DB_USER' => '', // 用户名
+  'DB_PWD' => '', // 密码
+  'DB_PORT' => 3306, // 端口
+  'DB_PREFIX' => '', // 数据库表前缀
+	'DB_CHARSET' => 'utf8',
+
+	/*
+	 下列设置请勿随意调节
+	 配置参考：http://document.thinkphp.cn/manual_3_2.html#config_reference
+	*/
 	'URL_MODEL'             =>  2,
 	'URL_HTML_SUFFIX'       =>  '',
 	'TMPL_TEMPLATE_SUFFIX'  =>  '.html',
