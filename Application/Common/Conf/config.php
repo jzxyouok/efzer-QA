8ae049b002a9d96be23ce98114dd8eb093b4e9ee
Yinan Zhou <zhouyinan@efzer.org>
2016-07-02 23:17:28 +0800

    Adjust indentation in config.php

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -13,10 +13,10 @@ return array(
 	'DB_TYPE' => 'mysql', //数据库类型
 	'DB_HOST' => 'localhost', //数据库地址
 	'DB_NAME' => '', // 数据库名
-  'DB_USER' => '', // 用户名
-  'DB_PWD' => '', // 密码
-  'DB_PORT' => 3306, // 端口
-  'DB_PREFIX' => '', // 数据库表前缀
+	'DB_USER' => '', // 用户名
+	'DB_PWD' => '', // 密码
+	'DB_PORT' => 3306, // 端口
+	'DB_PREFIX' => '', // 数据库表前缀
 	'DB_CHARSET' => 'utf8',
 
 	/*
