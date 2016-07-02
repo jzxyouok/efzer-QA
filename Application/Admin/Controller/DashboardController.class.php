2dc7e29f6b68f209cbef656fa9ea998f625dc975
Yinan Zhou <zhouyinan@efzer.org>
2016-07-02 22:57:30 +0800

    Add Question Management Page

--- Application/Admin/Controller/DashboardController.class.php
+++ Application/Admin/Controller/DashboardController.class.php
@@ -22,6 +22,52 @@ class DashboardController extends Controller{
     $this->display();
   }
 
+  public function _before_questions_management(){
+    $this->check_login();
+  }
+  public function questions_management(){
+    $Question = D('Questions');
+    if(IS_POST && IS_AJAX){
+      if(empty($_POST['action'])){
+        exit('Bad Request');
+      }
+      switch($_POST['action']){
+        case 'add':
+          if(empty($_POST['question_text'])||empty($_POST['question_choices']||empty($_POST['question_answer']))){
+            echo json_encode(array('status'=>'failed',reason=>'请填写所有字段'));
+            return;
+          }
+          $Question->question_text = I('post.question_text');
+          $Question->question_choices = implode('|',explode(PHP_EOL,I('post.question_choices')));
+          $Question->question_answer = I('post.question_answer');
+          $Question->find($Question->add());
+          $result['question_id'] = $Question->question_id;
+          $result['question_text'] = $Question->question_text;
+          $result['question_choices'] = implode(' | ',explode('|',$Question->question_choices));
+          $result['question_answer'] = $Question->question_answer;
+          echo json_encode(array('status'=>'success','result'=>$result));
+          return;
+        case 'delete':
+          if(empty($_POST['question_id'])){
+            exit(json_encode(array('status'=>'failed','reason'=>'没有传入 question_id')));
+          }
+          $Question->delete($_POST['question_id']);
+          echo json_encode(array('status'=>'success','question_id'=>$_POST['question_id']));
+          return;
+        default:
+          exit('Undefied Action');
+      }
+      return;
+    }
+    $this->assign('empty_message','<tr class="active"><th colspan="4">题库为空</th></tr>');
+    $questions = $Question->select();
+    for($i = 0; $i < count($questions); $i++){
+      $questions[$i]['question_choices'] = implode(' | ', explode('|', $questions[$i]['question_choices']));
+    }
+    $this->assign('questions', $questions);
+    $this->display();
+  }
+
   public function login(){
     if(IS_POST){
       if(empty($_POST['pwd'])){
