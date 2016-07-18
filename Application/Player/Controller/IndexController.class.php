f1ec70d5a4da3762cbaa382fa3d0ec032dacfdf5
Yinan Zhou <zhouyinan@efzer.org>
2016-07-18 15:05:34 +0800

    Add get_answer() function

--- Application/Player/Controller/IndexController.class.php
+++ Application/Player/Controller/IndexController.class.php
@@ -2,7 +2,15 @@
 namespace Player\Controller;
 use Think\Controller;
 class IndexController extends Controller{
-  public function index(){
-    echo "Play";
+
+  private function get_answer($question_id){
+    $cached_answer = F('Answer-' . $question_id);
+    if($cached_answer === false){
+      $Question = M('Questions');
+      $Question->find($question_id);
+      $cached_answer = $Question->question_answer;
+      F('Answer-' . $question_id, $cached_answer);
+    }
+    return $cached_answer;
   }
 }
