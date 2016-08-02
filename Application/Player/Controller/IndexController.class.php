0ec6a6ac6bf7e9404fd0bff0a641de17fec4039f
Yinan Zhou <zhouyinan@efzer.org>
2016-08-02 15:01:02 +0800

    Add score judging part

--- Application/Player/Controller/IndexController.class.php
+++ Application/Player/Controller/IndexController.class.php
@@ -5,12 +5,28 @@ class IndexController extends Controller{
 
   public function index(){
     $Question = D('Questions');
-    $Response =  D('Responses');
-    $Submission = D('Submissions');
+    $Submission = M('Submissions');
     if(IS_POST){
       $Submission->submission_id = uniqid();
       $Submission->submission_time = date('Y-m-d H:i:s');
-      
+      echo "<meta charset=\"utf-8\">";
+      foreach($_POST['question'] as $k => $response){
+        $Response =  M('Responses');
+        var_dump($response);
+        $Response->submission_id = $Submission->submission_id;
+        $Response->question_id = $response['question_id'];
+        echo $this->get_answer($Response->question_id);
+        $Response->response_choice = $response['answer'];
+        $Response->correct = ($Response->response_choice == $this->get_answer($Response->question_id))?1:0;
+        echo '<br />';
+        echo $Response->correct;
+        $Response->add();
+      }
+      $Response = D('Responses');
+      $condition['submission_id'] = $Submission->submission_id;
+      $Submission->score = $Response->where($condition)->sum('correct');
+      $Submission->add();
+      echo $Submission->score;
     }
     else{
       $questions = $Question->select();
@@ -19,7 +35,7 @@ class IndexController extends Controller{
       $questions_display = array();
       foreach($questions as $k => $question){
         $questions_display[$k] = $question;
-        $questions_display[$k]['question_choices'] = explode('|',$question['question_choices']);
+        $questions_display[$k]['question_choices'] = array_map("trim",explode('|',$question['question_choices']));
       }
       $this->assign('question_list',$questions_display);
       $this->display();
