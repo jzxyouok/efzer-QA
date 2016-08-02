e8d0230548fc970e7934af645507d35aa01e2b38
Yinan Zhou <zhouyinan@efzer.org>
2016-08-02 21:40:19 +0800

    Add PK function

--- Application/Player/Controller/IndexController.class.php
+++ Application/Player/Controller/IndexController.class.php
@@ -4,10 +4,19 @@ use Think\Controller;
 class IndexController extends Controller{
 
   public function index(){
+    $this->assign('sign',$this->sign_js_api());
     $Question = D('Questions');
     $Submission = M('Submissions');
     if(cookie('submission_id')){
-      $this->show_result(cookie('submission_id'));
+      if(empty($_GET['from_id'])){
+        redirect('/?from_id=' . cookie('submission_id'));
+      }
+      elseif($_GET['from_id'] != cookie('submission_id')){
+        $this->player_pk(cookie('submission_id'),$_GET['from_id']);
+      }
+      else{
+        $this->show_result(cookie('submission_id'));
+      }
     }
     elseif(IS_POST){
       $Submission->submission_id = uniqid();
@@ -25,7 +34,7 @@ class IndexController extends Controller{
       $Submission->score = $Response->where($condition)->sum('correct');
       $Submission->add();
       cookie('submission_id',$condition['submission_id'],518400);
-      $this->show_result($condition['submission_id']);
+      redirect('/?from_id=' . $condition['submission_id']);
     }
     else{
       $questions = $Question->select();
@@ -42,9 +51,10 @@ class IndexController extends Controller{
   }
 
   private function show_result($submission_id){
+    $this->assign('sign',$this->sign_js_api());
     $Submission = M('Submissions');
     $Submission->find($submission_id);
-    $score = $Submission->score;
+    $score = (int)$Submission->score;
     $upper_condition['score'] = array('gt',$score);
     $upper_count = $Submission->where($upper_condition)->count();
     $total_count = $Submission->count();
@@ -78,6 +88,51 @@ class IndexController extends Controller{
       F('Answer-' . $question_id, $cached_answer);
     }
     return $cached_answer;
+  }
+
+  private function player_pk($origin,$target){
+    $condition['submission_id'] = $origin;
+    $OriginSubmission = M('Submissions')->where($condition)->find();
+    $condition['submission_id'] = $target;
+    $TargetSubmission = M('Submissions')->where($condition)->find();
+    $origin_score = $OriginSubmission['score'];
+    $target_score = $TargetSubmission['score'];
+    if($origin_score > $target_score){
+      $this->assign('result','success');
+    }
+    elseif($origin_score < $target_score){
+      $this->assign('result','failed');
+    }
+    else{
+      $this->assign('result','draw');
+    }
+    $this->assign('origin_correction',round(100 * $origin_score / 15));
+    $this->assign('target_correction',round(100 * $target_score / 15));
+    $this->display('pk');
+  }
+
+  private function sign_js_api(){
+    $sign['jsapi_ticket'] = $this->get_jsapi_ticket();
+    $sign['noncestr'] = uniqid();
+    $sign['timestamp'] = time();
+    $sign['signature'] = sha1(http_build_query($sign) . '&url=https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+    $sign['appID'] = 'wx34f82eb8c3e01bb4';
+    return $sign;
+  }
+
+  private function get_jsapi_ticket(){
+    if(S('js_api_ticket') == false){
+      $ch = curl_init();
+      $curl_options = array(
+        CURLOPT_URL => 'http://wechat.efzer.org/efzaa/fetch_js_api_ticket',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_TIMEOUT => 10,
+      );
+      curl_setopt_array($ch,$curl_options);
+      $result = json_decode(curl_exec($ch),true);
+      S('js_api_ticket',$result['ticket'],$result['expire'] - time());
+    }
+    return S('js_api_ticket');
   }
 
   public function remove_cookie(){
