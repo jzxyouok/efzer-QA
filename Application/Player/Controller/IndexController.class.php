c74ab801f7089202ef6f58dd39202717ddcac457
Yinan Zhou <zhouyinan@efzer.org>
2016-08-02 14:26:15 +0800

    Add index() function for question display

--- Application/Player/Controller/IndexController.class.php
+++ Application/Player/Controller/IndexController.class.php
@@ -3,6 +3,29 @@ namespace Player\Controller;
 use Think\Controller;
 class IndexController extends Controller{
 
+  public function index(){
+    $Question = D('Questions');
+    $Response =  D('Responses');
+    $Submission = D('Submissions');
+    if(IS_POST){
+      $Submission->submission_id = uniqid();
+      $Submission->submission_time = date('Y-m-d H:i:s');
+      
+    }
+    else{
+      $questions = $Question->select();
+      shuffle($questions);
+      $questions = array_slice($questions,0,15);
+      $questions_display = array();
+      foreach($questions as $k => $question){
+        $questions_display[$k] = $question;
+        $questions_display[$k]['question_choices'] = explode('|',$question['question_choices']);
+      }
+      $this->assign('question_list',$questions_display);
+      $this->display();
+    }
+  }
+
   private function get_answer($question_id){
     $cached_answer = F('Answer-' . $question_id);
     if($cached_answer === false){
